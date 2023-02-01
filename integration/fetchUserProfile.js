f39080010383160f28a9d76df19408581d39cdb7
Sam Yapkowitz <samyapkowitz@gmail.com>
2023-02-01 15:40:06 -0600

    image fixes

--- integration/fetchUserProfile.js
+++ integration/fetchUserProfile.js
@@ -1,26 +1,26 @@
 /* globals request */
 module.exports = function fetchUserProfile(accessToken, context, cb) {
-  const axios = require('axios@0.22.0');
-  const userInfoEndpoint = 'https://kit.snapchat.com/v1/me';
-  const headers = { 'Authorization': 'Bearer ' + accessToken };
+  const axios = require("axios@0.22.0");
+  const userInfoEndpoint = "https://kit.snapchat.com/v1/me";
+  const headers = { Authorization: `Bearer ${accessToken}` };
   const query = "{me{displayName bitmoji{avatar} externalId}}";
 
   axios
     .post(userInfoEndpoint, { query }, { headers })
-    .then(res => {
+    .then((res) => {
       if (res.status !== 200) {
         return cb(new Error(res.data));
       }
-	  
-      const user_data = res.data.data.me;
-    
+
+      const userData = res.data.data.me;
+
       const profile = {
-        user_id: user_data.externalId,
-        nickname: user_data.displayName,
-        picture: user_data.bitmoji.avatar
+        user_id: userData.externalId,
+        nickname: userData.displayName,
+        picture: userData.bitmoji.avatar,
       };
-      
+
       cb(null, profile);
     })
-    .catch(err => cb(err));
+    .catch((err) => cb(err));
 };
